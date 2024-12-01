a4f327831fec6a4c36e5ad92e15b1009d6e39544
Sagnac <83491030+Sagnac@users.noreply.github.com>
2024-12-01 02:00:32 +0000

    Add some `complex_plot` methods
    The ModArg method is there for convenience; it sets the unit disk,
    zeroing out values which fall outside of it for plotting as having the
    forms / functions return zero for q ≥ 1 is technically incorrect and
    improper and restricting their domain is impractical.
    
    The tau methods are necessary to resolve dispatch ambiguities since `f`
    is generic at ComplexColor. I could just dispatch with an extra ModArg
    argument to avoid redundancy, but passing along an extra type when
    unnecessary is unintuitive and that also leaves unbounded f(q) methods
    available; it's a small price to pay to keep the interface clean and
    simple.

--- src/ModularForms.jl
+++ src/ModularForms.jl
@@ -1,15 +1,16 @@
 module ModularForms
 
 using ComplexColor
-import ComplexColor: GLMakie.Screen, complex_plot
+import ComplexColor: GLMakie.Screen, complex_plot, Interval, ComplexArray
 using SpecialFunctions: zeta
 
-export ModularForm, ModularFunction, S, G, E, j, q, complex_plot
+export ModularForm, ModularFunction, S, G, E, j, q, complex_plot, ReIm, ModArg
 
 const ∑ = sum
 const ∏ = prod
 const ζ = zeta
 const t = Ref(275)
+const n = Ref(500)
 
 q(τ) = cispi(2τ)
 
@@ -21,6 +22,9 @@ end
 abstract type ModularForm end
 
 abstract type EisensteinSeries <: ModularForm end
+
+struct ReIm end
+struct ModArg end
 
 # Eisenstein series
 struct G <: EisensteinSeries
@@ -91,6 +95,9 @@ j_invariant(q::Number, t::Int = t[]) = E4(q, t) ^ 3 / Δ(q, t)
 
 j = ModularFunction(j_invariant)
 
+set_title(f::T) where T <: EisensteinSeries = string(T, "_", f.k)
+set_title(f::ModularForm) = f ≡ j ? "j" : "f"
+
 function logclamp!(w)
     max_w = maximum(filter(isfinite, w))
     map!(w, w) do x
@@ -98,8 +105,7 @@ function logclamp!(w)
     end
 end
 
-function complex_plot(f::ModularForm, t::Int = t[], n::Int = 500)
-
+function complex_plot(f::ModularForm, ::Type{ReIm}, t::Int = t[], n::Int = n[])
     x = y = range(-1, 1, n)
     q = complex.(x, y')
     M = f.(q, t)
@@ -109,17 +115,58 @@ function complex_plot(f::ModularForm, t::Int = t[], n::Int = 500)
     logclamp!(v)
     figure = ComplexColor.Figure(; size = (2n, n))
     titlesize = 21
-    u_axis = ComplexColor.Axis(figure[1,1]; title = L"Re_+(f(q))", titlesize)
-    v_axis = ComplexColor.Axis(figure[1,2]; title = L"Im_+(f(q))", titlesize)
+    f_title = set_title(f)
+    u_axis = ComplexColor.Axis(figure[1,1]; title = L"Re_+(%$f_title(q))", titlesize)
+    v_axis = ComplexColor.Axis(figure[1,2]; title = L"Im_+(%$f_title(q))", titlesize)
     ComplexColor.hidedecorations!(u_axis)
     ComplexColor.hidedecorations!(v_axis)
     kwargs = (; interpolate = false, colormap = :pink)
-
     ComplexColor.image!(u_axis, u; kwargs...)
     ComplexColor.image!(v_axis, v; kwargs...)
-
     figure
+end
 
+function complex_plot(f::ModularForm, t::Int = t[], n::Int = n[])
+    complex_plot(f, ReIm, t, n)
+end
+
+function complex_plot(f::ModularForm, ::Type{ModArg},
+                      color::ComplexColor.Spaces = ComplexColor.default,
+                      t::Int = t[], n::Int = n[]; title = L"%$(set_title(f))(q)")
+    x = y = range(-1, 1, n)
+    complex_plot(x, y, q -> abs(q) < 1 ? f(q, t) : 0.0im, color; title)
+end
+
+# f(τ) methods
+
+function complex_plot(x::AbstractVector, y::AbstractVector, f::ModularForm,
+                      color::ComplexColor.Spaces = ComplexColor.default;
+                      title = L"%$(set_title(f))(τ)")
+    complex_plot(x, y, f ∘ q, color; title)
+end
+
+function complex_plot(x::Interval, y::Interval, f::ModularForm,
+                      color::ComplexColor.Spaces = ComplexColor.default;
+                      title = L"%$(set_title(f))(τ)")
+    complex_plot(x, y, f ∘ q, color; title)
+end
+
+function complex_plot(x::AbstractVector, y::Interval, f::ModularForm,
+                      color::ComplexColor.Spaces = ComplexColor.default;
+                      title = L"%$(set_title(f))(τ)")
+    complex_plot(x, y, f ∘ q, color; title)
+end
+
+function complex_plot(x::Interval, y::AbstractVector, f::ModularForm,
+                      color::ComplexColor.Spaces = ComplexColor.default;
+                      title = L"%$(set_title(f))(τ)")
+    complex_plot(x, y, f ∘ q, color; title)
+end
+
+function complex_plot(z::ComplexArray, f::ModularForm,
+                      color::ComplexColor.Spaces = ComplexColor.default;
+                      title = L"%$(set_title(f))(τ)")
+    complex_plot(z, f ∘ q, color; title)
 end
 
 end
